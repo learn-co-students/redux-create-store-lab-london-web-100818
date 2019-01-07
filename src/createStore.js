abd18545133028cb0fb777d612d5f2dc41709a97
lola <slonanuga@gmail.com>
2019-01-07 14:36:32 +0000

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,7 +1,23 @@
 export default function createStore(reducer) {
-  // add your code here
-}
+  
+    let state;
+
+    const dispatch = (action = { type: '@@INIT' }) => {
+      state = reducer(state, action )
+      render()
+    }
+
+    const getState = () => {
+      return state
+    }
+   
+    dispatch({ type: '@@INIT' })
+
+    return {  dispatch,getState}
+  }
+
 
 function render() {
   const container = document.getElementById('container');
+
 }
